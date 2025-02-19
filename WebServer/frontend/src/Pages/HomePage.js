c19707c25e938a743955fce5ff6af90956c0b52e
hannakoshy3 <hmk4@njit.edu>
2025-02-19 14:06:05 -0500

    updated homepage

--- WebServer/frontend/src/Pages/HomePage.js
+++ WebServer/frontend/src/Pages/HomePage.js
@@ -16,13 +16,12 @@ function HomePage() {
         <h1>
             Home Page
         </h1>
-        <div class = "profile-pic">
-            <label class="upload-label" for="file">
-                <span class="glyphicon glyphicon-camera">=
-                </span>
+        <div className = "profile-pic">
+            <label className ="upload-label" htmlFor="file">
+                <span class="glyphicon glyphicon-camera">+</span>
             </label>
             <input id="file" type="file" onChange={handleImageChange} style ={{display:"none"}}/>
-            <img src = {image || "/home/hanna/Downloads/istockphoto-1300845620-612x612.jpg"} id="output" alt="Profile" width="200" />
+            <img src = {image || "https://example.com/default-profile.jpg"} id="output" alt="Profile" width="200" />
            </div>
         </div>
     );
