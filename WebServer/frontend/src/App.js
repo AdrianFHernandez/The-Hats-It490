f1c974a78c2a3153503d22f4c773e35e9aa67813
hannakoshy3 <hmk4@njit.edu>
2025-02-20 12:47:54 -0500

    homepage

--- WebServer/frontend/src/App.js
+++ WebServer/frontend/src/App.js
@@ -3,19 +3,44 @@ import './App.css';
 import RegisterPage from './Pages/RegisterPage';
 import LoginPage from './Pages/LoginPage';
 import { useState } from "react";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-  const [registering, setRegistering] = useState(false)
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  }
+
+  const handleRegistration = () => {
+    setIsRegistered(true);
+    setRegistering(false);
+  }
   
 
   return (
     <div className="App">
+      <p>hello there</p>
+
+      {loggedIn ?(
+
+      <HomePage user={{name:"Tsewang", email:"email"}}></HomePage>)
+      : registering ? (
+        <RegisterPage onRegister={handleRegistration} />
+      ) : (
+        <LoginPage onLogin = {handleLogin} />
+      )
+    }
+
+      
       <h2>InvestZero!</h2>
       <h4>Practice investing for free and learn to grow your wealth!</h4>
       {registering ? <RegisterPage /> : <LoginPage />}
       <button onClick={() => setRegistering(!registering)}>
         {registering ? "Already have an account? Login here!" : "Don't have an account? Register here!"}    
-      </button>
+      </button> 
     </div>
   );
 }
