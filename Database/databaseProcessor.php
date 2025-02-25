099f97cfdc37e615cb7669aa5892e2c4e80e1bac
Adrian <afh23@njit.edu>
2025-02-25 18:01:05 -0500

    CLean and orginize Database folder

---
+++ Database/databaseProcessor.php
@@ -0,0 +1,46 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('databaseModule.php');
+
+
+
+function requestProcessor($request)
+{
+    echo "received request".PHP_EOL;
+    var_dump($request);
+
+    
+    if (!isset($request['type'])) {
+        return ["returnCode" => '3', "message" => "ERROR: unsupported message type"];
+    }
+
+    switch ($request['type']) {
+        case "register":
+            return doRegister($request['name'], $request['username'], $request['email'], $request['password']);
+        case "login":
+            $response = doLogin($request['username'], $request['password']);
+            if ($response["returnCode"] === '0') {
+                $sessionData = createSession($response["user"]["id"]);
+                $response["session"] = $sessionData;
+                clearExpiredSessions();
+            }
+            return $response;
+        case "validateSession":
+            return validateSession($request['sessionId']);
+        case "logout":
+            return doLogout($request['sessionId']);
+        default:
+            return ["returnCode" => '3', "message" => "Unsupported message type"];
+    }
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+echo "testRabbitMQServer BEGIN".PHP_EOL;
+$server->process_requests('requestProcessor');
+echo "testRabbitMQServer END".PHP_EOL;
+exit();
+?>
