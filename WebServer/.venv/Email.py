5959c5ab0990c26a0960c73d274eaec80e8b5c80
hanna <hanna@IT490-002.myguest.virtualbox.org>
2025-03-18 12:21:25 -0400

    updates to Email.py

---
+++ WebServer/.venv/Email.py
@@ -0,0 +1,25 @@
+import sendgrid
+from sendgrid.helpers.mail import Mail
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+sendgrid_api=os.getenv("SENDGRID_API_KEY")
+
+def send_email(to, subject, content):
+    sg = sendgrid.SendGridAPIClient(sendgrid_api)
+    email = Mail(
+        from_email=("490hatsit@gmail.com", "Stock Site"),
+        to_emails=to,
+        subject=subject,
+        plain_text_content=content
+    )
+
+    try:
+        response = sg.send(email)
+        return response.status_code
+    except Exception as e:
+        print(f'Caught exception: {str(e)}')
+
+# Example usage:
+# send_email("recipient@example.com", "Test Subject", "This is a test email.")
