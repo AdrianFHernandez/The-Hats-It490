be4359ef62f361b01a76fe88ba4e74e7a0b9c81f
Adrian <afh23@njit.edu>
2025-04-09 14:56:57 -0400

    Distrubted login

---
+++ Deployment/LOG/LogServer.php
@@ -0,0 +1,30 @@
+#!/usr/bin/php
+<?php
+
+require_once("rabbitMQLoggingLib.inc");
+
+
+function requestProcessor($request) {
+
+    print_r($request);
+    if (!isset($request['type'])) {
+        return buildResponse("UNKNOWN", "ERROR", ["message" => "Missing type"]);
+    }
+
+
+    switch ($request['type']) {
+        case "Error":
+            return ["log"=>"ts"];
+         default:
+            break;
+            
+    }
+
+    return buildResponse($request['type'], "UNKNOWN", ["message" => "Unhandled type"]);
+}
+
+echo "Deployment Server Started\n";
+$server = new rabbitMQServer("DistributedLogginRabbitMQ.ini", "DEVDistributedLogginServer");
+$server->process_requests('requestProcessor');
+echo "Deployment Server Ended\n";
+?>
