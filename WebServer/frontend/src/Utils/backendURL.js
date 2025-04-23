13f1eb69a808d622e233a08cb60d655c0fb3f68c
Tsewang Sherpa <tds22@njit.edu>
2025-04-23 16:35:47 -0400

    Fixed cors erros and now it works with SSL

--- WebServer/frontend/src/Utils/backendURL.js
+++ WebServer/frontend/src/Utils/backendURL.js
@@ -1,6 +1,6 @@
 const getBackendURL = () => {
     // return "http://www.sample.com/backend/webserver_backend.php";
-    return "https://100.70.255.118/backend/webserver_backend.php";
+    return "https://www.investzero.com/backend/webserver_backend.php";
 
     
 };
