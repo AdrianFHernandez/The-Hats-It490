dcd9b002a2fe777f2fcbffb56528b9769fca7b46
hanna <hanna@IT490-002.myguest.virtualbox.org>
2025-04-23 13:46:13 -0400

    changes to logging and request handling

--- DIstrubted login/DEV_Deployment/DB login/LogServer.php
+++ DIstrubted login/DEV_Deployment/DB login/LogServer.php
@@ -33,11 +33,12 @@ function requestProcessor($request) {
     
     global $logFile, $logError;
     $timestamp = date("D M d H:i:s Y");
-    $logEntry = "[$timestamp] " . json_encode($request) . "\n";
+    $logEntry = "[$timestamp] " . "--" .
+$request["message"] . "\n";
 
     // Determine where to log
     if (isset($request['type']) && $request['type'] === "Error") {
-        file_put_contents($errorLogFile, $logEntry, FILE_APPEND);
+        file_put_contents($logError, $logEntry, FILE_APPEND);
     } else {
         file_put_contents($logFile, $logEntry, FILE_APPEND);
     }
