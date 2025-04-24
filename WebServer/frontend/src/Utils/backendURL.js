e797005a955c89337e95f414b01c24ffbd398111
Hector Saucedo <hs78@njit.edu>
2025-04-24 15:48:15 +0000

    updated pass in deployment 4 DEV

--- WebServer/frontend/src/Utils/backendURL.js
+++ WebServer/frontend/src/Utils/backendURL.js
@@ -1,6 +1,6 @@
 const getBackendURL = () => {
     // return "http://www.sample.com/backend/webserver_backend.php";
-    return "https://100.70.255.118/backend/webserver_backend.php";
+    return "https://100.81.56.68/backend/webserver_backend.php";
 
     
 };
