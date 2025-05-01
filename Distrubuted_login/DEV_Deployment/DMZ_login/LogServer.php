d6f102da2fdab440e7fefd9edcbb8360977d1752
Tsewang Sherpa <tds22@njit.edu>
2025-05-01 12:30:13 -0400

    LogServer is now consistent with message saving

--- Distrubuted_login/DEV_Deployment/DMZ_login/LogServer.php
+++ Distrubuted_login/DEV_Deployment/DMZ_login/LogServer.php
@@ -3,7 +3,7 @@
 
 require_once("rabbitMQLib.inc");
 
-    $logDir = "/var/log/DistributedLogging/";//cd /var/log/    sudo mkdir LogResponse"
+    $logDir = "/var/log/DistributedLogging/";
     $logFile = $logDir . "DistributedInvestZero.log";
     $logError = $logDir . "DistributedInvestZero.err";
 
@@ -11,11 +11,11 @@ function requestProcessor($request) {
     // Base log file paths
    print_r($request["message"]); 
 	global $logFile, $logError;
-    $timestamp = date("D M d H:i:s Y");
-    $logEntry = "[$timestamp] " . $request["message"] . "\n";
+    $timestamp = $request["timestamp"];
+    $logEntry = "[$timestamp] " . " -- " . $request["message"] . "\n";
 
     // Determine where to log
-    if (isset($request['type']) && $request['type'] === "ERROR") {
+    if (isset($request['type']) && $request['type'] === "Error") {
         file_put_contents($logError, $logEntry, FILE_APPEND);
     } else {
         file_put_contents($logFile, $logEntry, FILE_APPEND);
--- WebServer/backend/LogServer.php
+++ WebServer/backend/LogServer.php
@@ -7,16 +7,15 @@ require_once("rabbitMQLib.inc");
     $logFile = $logDir . "DistributedInvestZero.log";
     $logError = $logDir . "DistributedInvestZero.err";
 
-    
 function requestProcessor($request) {
     // Base log file paths
-   print_r($request); 
+   print_r($request["message"]); 
 	global $logFile, $logError;
-    $timestamp = date("D M d H:i:s Y");
+    $timestamp = $request["timestamp"];
     $logEntry = "[$timestamp] " . " -- " . $request["message"] . "\n";
 
     // Determine where to log
-    if (isset($request['type']) && $request['type'] === "ERROR") {
+    if (isset($request['type']) && $request['type'] === "Error") {
         file_put_contents($logError, $logEntry, FILE_APPEND);
     } else {
         file_put_contents($logFile, $logEntry, FILE_APPEND);
